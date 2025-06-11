83cd50e325d6e8ee838ac581e1797a57c6e9056a
DDI-droid <deepdecisionintelligence.com>
2025-06-11 11:36:20 +0530

    poly_time env

--- pufferlib/ocean/environment.py
+++ pufferlib/ocean/environment.py
@@ -147,6 +147,7 @@ MAKE_FUNCTIONS = {
     'impulse_wars': 'ImpulseWars',
     'gpudrive': 'GPUDrive',
     'pacman': 'Pacman',
+    'poly_time': 'PolyTime',
     'spaces': make_spaces,
     'multiagent': make_multiagent,
 }
